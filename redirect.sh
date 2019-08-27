ebc6aa1377118932f5921c8595e14f318ef4136d
Benjamin Friesen <benjamin.friesen@student.ufv.ca>
2019-08-26 22:13:50 -0700

    redirect_to instead of meta

--- redirect.sh
+++ redirect.sh
@@ -4,8 +4,8 @@ rm -f redirects/$1.html
 cat <<EOF >> redirects/$1.html
 ---
 redirect_from: /$1
+redirect_to: $2
 ---
-<meta http-equiv='refresh' content='0; url=$2'>
 EOF
 git add redirects/$1.html
 git commit -q -m "$1 -> $2"
