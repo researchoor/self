3141deac0abcd91f9c97bbad4e9143d82df1e51f
Ben Friesen <bfriesenwork@gmail.com>
2021-04-24 21:55:53 -0700

    Refactored layouts and posts

--- redirect.sh
+++ redirect.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-rm -f redirects/$1.html
-cat <<EOF >> redirects/$1.html
+rm -f _redirects/$1.html
+cat <<EOF >> _redirects/$1.html
 ---
 redirect_from: /$1
 redirect_to: $2
 ---
 EOF
-git add redirects/$1.html
+git add _redirects/$1.html
 git commit -q -m "$1 -> $2"
 git push -q
