1c5b0fc6fe823c793ba0a1a54dcde1bb90b27806
Ben Friesen <bfriesenwork@gmail.com>
2021-04-27 18:02:56 -0700

    Tag cleanup

--- redirect.sh
+++ redirect.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-rm -f _redirects/$1.html
-cat <<EOF >> _redirects/$1.html
+rm -f _links/$1.html
+cat <<EOF >> _links/$1.html
 ---
 redirect_from: /$1
 redirect_to: $2
 category: ${3:?}
 ---
 EOF
-git add _redirects/$1.html
+git add _links/$1.html
 git commit -q -m "$1 -> $2"
 git push -q
