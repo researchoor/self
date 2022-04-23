5c0d482c5aaf86efe5fc8199d60d829cbf7bff99
resloved <bfriesenwork@gmail.com>
2022-04-23 12:43:18 -0700

    update redirect to collections folder

--- redirect.sh
+++ redirect.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-rm -f _links/$1.html
-cat <<EOF >> _links/$1.html
+rm -f collections/_links/$1.html
+cat <<EOF >> collections/_links/$1.html
 ---
 redirect_from: /$1
 redirect_to: $2
 category: ${3:?}
 ---
 EOF
-git add _links/$1.html
+git add collections/_links/$1.html
 git commit -q -m "$1 -> $2"
 git push -q
