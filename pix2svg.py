09b873290818aae31b7c4781f7ec98fb644b4671
resloved <bfriesenwork@gmail.com>
2022-11-28 12:27:24 -0800

    Overhaul

--- pix2svg.py
+++ pix2svg.py
@@ -2,22 +2,22 @@ import sys
 from PIL import Image
 
 pix = sys.argv[1]
-im = Image.open(pix, 'r').convert('RGB')
+im = Image.open(pix, 'r')
 width, height = im.size
 pixels = im.load()
 print(width, height)
 file_name = f"{'.'.join(pix.split('.')[:-1])}.svg"
 
-def test(name):
+def convert(name):
     with open(name, 'w') as f:
         f.write(f"<svg xmlns='http://www.w3.org/2000/svg' viewbox='0 0 {width} {height}'>\n")
         for i in range(width):
             for j in range(height):
                 print(pixels[i, j], i, j)
-                if pixels[i, j] == (0, 0, 0):
+                if pixels[i, j] == (0, 0, 0, 255):
                     f.write(f"<rect width='1' height='1' x='{i}' y='{j}'></rect>\n")
                 elif pixels[i, j] != (153, 204, 204):
                     f.write(f"<rect width='1' height='1' x='{i}' y='{j}' fill='rgb{pixels[i, j]}'></rect>\n")
         f.write(f"</svg>")
 
-test(file_name)
+convert(file_name)
